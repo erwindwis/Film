999d540301fdcc74ca93ec90b9a558cc29fa23f9
ALVIN KAMAL <alvinkn.dev@gmail.com>
2022-12-07 15:09:42 +0700

    categories done

--- app/src/main/java/com/example/film/Category.java
+++ app/src/main/java/com/example/film/Category.java
@@ -2,13 +2,22 @@ package com.example.film;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.ConsoleMessage;
+import android.widget.TextView;
+
+import java.io.Console;
 
 public class Category extends AppCompatActivity {
-
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String genre = intent.getStringExtra("genre");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        title = findViewById(R.id.tvCategory);
+        title.setText(genre);
     }
 }
