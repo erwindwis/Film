c4f8288d61984d3475780db325fd87127ee60227
ALVIN KAMAL <alvinkn.dev@gmail.com>
2022-12-07 14:18:04 +0700

    login done

--- app/src/main/java/com/example/film/Login.java
+++ app/src/main/java/com/example/film/Login.java
@@ -1,7 +1,9 @@
 package com.example.film;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
@@ -11,18 +13,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
 
     EditText email;
     EditText password;
     Button loginButton;
     boolean passwordVisible;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        auth = FirebaseAuth.getInstance();
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
@@ -58,9 +67,27 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //if success
-                Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                //else
-                Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                login(email.getText().toString(), password.getText().toString());
+            }
+        });
+
+    }
+    void login(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    task.getResult().getUser();
+                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    FirebaseUser user = task.getResult().getUser();
+                    i.putExtra("user",user);
+                    startActivity(i);
+                    //else
+
+                }else{
+                    Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/film/MainActivity.java
+++ app/src/main/java/com/example/film/MainActivity.java
@@ -3,14 +3,37 @@ package com.example.film;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton addButton;
+    TextView petualangan, keluarga, misteri, fantasi, romantis, animasi, aksi, horor, drama, komedi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButton = findViewById(R.id.addButton);
+        petualangan = findViewById(R.id.petualangan);
+        keluarga = findViewById(R.id.keluarga);
+        misteri = findViewById(R.id.misteri);
+        fantasi = findViewById(R.id.fantasi);
+        romantis = findViewById(R.id.romantis);
+        animasi = findViewById(R.id.animasi);
+        aksi = findViewById(R.id.aksi);
+        horor = findViewById(R.id.horor);
+        drama = findViewById(R.id.drama);
+        komedi = findViewById(R.id.komedi);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
